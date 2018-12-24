b1372fc90d033f46aaacbb2cd4eeb204e6fd09d6
alirzasahb <alireza.sahebi7@gmail.com>
2018-12-24 15:33:31 +0330

    Fix Typo

--- Check_dotNetCore_SDK_Installed_Version.ps1
+++ Check_dotNetCore_SDK_Installed_Version.ps1
@@ -1,3 +1,3 @@
-Write-Host "NET Core Inistalled SDK(s):" 
+Write-Host "NET Core Installed SDK(s):" 
 (dir (Get-Command dotnet).Path.Replace('dotnet.exe', 'sdk')).Name
 Read-Host -Prompt “Press Any Key To Exit...”
